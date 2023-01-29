ffd5f3a9ce7b05d1ba3e8c37862fb87c56381d50
Biswajit Sharma <biswajitsharmait@gmail.com>
2023-01-29 12:05:18 +0530

    add media query

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -4,39 +4,43 @@ import "./ContactForm.css";
 
 const ContactForm = () => {
   const [state, handleSubmit] = useForm("xoqzjerz");
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
-  }
+  const form = (
+    <React.Fragment>
+      <div className="title">
+        <p className="content">Any query, leave a message</p>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="input">
+          <span>You email address</span>
+          <input id="email" type="email" name="email" />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <div className="input">
+          <span>Description</span>
+          <textarea id="message" name="message" />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+      </form>
+    </React.Fragment>
+  );
+  const successData = (
+    <div className="response">
+      <div className="title">Query Sent!</div>
+      <div className="description">Thank you for reaching out, you will be responded within 12hours</div>
+    </div>
+  );
+  let mainData = state.succeeded ? successData : form;
+
   return (
     <div className="form-container">
-      <div className="form">
-        <div className="title">
-          <p className="content">Any query, leave a message</p>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="input">
-            <span>You email address</span>
-            <input id="email" type="email" name="email" />
-            <ValidationError
-              prefix="Email"
-              field="email"
-              errors={state.errors}
-            />
-          </div>
-          <div className="input">
-            <span>Description</span>
-            <textarea id="message" name="message" />
-            <ValidationError
-              prefix="Message"
-              field="message"
-              errors={state.errors}
-            />
-          </div>
-          <button type="submit" disabled={state.submitting}>
-            Submit
-          </button>
-        </form>
-      </div>
+      <div className="form">{mainData}</div>
     </div>
   );
 };
